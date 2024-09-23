701cb48f7a50077f4e4e20768f1050d213153016
gvoltr <gvoltr@gmail.com>
2024-09-23 17:27:01 +0200

    deps cleanup

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     // Codegen
     id("com.google.devtools.ksp") version "2.0.20-1.0.25"
     id("de.jensklingenberg.ktorfit") version "2.1.0"
-    kotlin("plugin.serialization") version "2.0.20"
+    alias(libs.plugins.kotlin.serialization)
 }
 
 kotlin {
